eb7073292e53e2c3b7e5b08ffd5be4b2030ffd5a
numpon014 <numpon014@gmail.com>
2021-12-13 12:57:34 +0700

    change profile card to ediable

--- app/components/ProfileCard/index.js
+++ app/components/ProfileCard/index.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { memo } from 'react';
 import PropTypes from 'prop-types';
 import { Card } from 'react-bootstrap';
 import styled from 'styled-components';
+import EasyEdit, { Types } from 'react-easy-edit';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { FormattedMessage, injectIntl } from 'react-intl';
+import { updateUser } from 'shares/actions/users';
 
 const StyledWrapper = styled.div`
   .profile {
@@ -20,8 +27,18 @@ const StyledWrapper = styled.div`
       }
     }
 
-    h5.name {
+    .name {
       margin: 0 0 10px 0;
+    }
+
+    .inline-editor {
+      &.name {
+        margin: 0 0 10px 0;
+      }
+      &.age {
+        font-size: 0.8rem;
+        margin: 10px 0 0;
+      }
     }
 
     h6.email {
@@ -33,7 +50,17 @@ const StyledWrapper = styled.div`
   }
 `;
 
-function ProfileCard({ className, user }) {
+function ProfileCard({ className, user, onSubmitForm, intl }) {
+  const handleSubmit = (name, value) => {
+    onSubmitForm(
+      user.id,
+      {
+        [name]: value,
+      },
+      intl.formatMessage({ id: 'loginPage.form.result.fail.message' }),
+    );
+  };
+
   return (
     <StyledWrapper className={className}>
       <Card>
@@ -44,8 +71,34 @@ function ProfileCard({ className, user }) {
                 <div className="avatar">
                   <img src={user.avatar.url} alt={user.name} />
                 </div>
-                <h5 className="name">{user.name}</h5>
+                <div className="inline-editor name">
+                  <EasyEdit
+                    type={Types.TEXT}
+                    value={user.name}
+                    onSave={value => {
+                      handleSubmit('name', value);
+                    }}
+                    saveButtonLabel={<FontAwesomeIcon icon={faCheck} />}
+                    cancelButtonLabel={<FontAwesomeIcon icon={faTimes} />}
+                    attributes={{ name: 'name', id: 4 }}
+                  />
+                </div>
                 <h6 className="email">{user.username}</h6>
+                <div className="inline-editor age">
+                  <span>
+                    <FormattedMessage id="profilePage.form.field.age.label" />
+                  </span>
+                  <EasyEdit
+                    type={Types.TEXT}
+                    value={user.age}
+                    onSave={value => {
+                      handleSubmit('age', value);
+                    }}
+                    saveButtonLabel={<FontAwesomeIcon icon={faCheck} />}
+                    cancelButtonLabel={<FontAwesomeIcon icon={faTimes} />}
+                    attributes={{ name: 'age', id: 4 }}
+                  />
+                </div>
               </div>
             </div>
           )}
@@ -58,6 +111,23 @@ function ProfileCard({ className, user }) {
 ProfileCard.propTypes = {
   className: PropTypes.string,
   user: PropTypes.object,
+  onSubmitForm: PropTypes.func,
+  intl: PropTypes.object,
 };
 
-export default ProfileCard;
+const mapDispatchToProps = dispatch => ({
+  onSubmitForm(id, params, failMessage, callback) {
+    dispatch(updateUser(id, params, failMessage, callback));
+  },
+});
+
+const withConnect = connect(
+  null,
+  mapDispatchToProps,
+);
+
+export default compose(
+  injectIntl,
+  withConnect,
+  memo,
+)(ProfileCard);
